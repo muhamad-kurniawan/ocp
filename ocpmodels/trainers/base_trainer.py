94d991891441afef10989e1aed7f4cbce027df07
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2023-08-17 15:30:55 +0900

    Update base_trainer.py

--- ocpmodels/trainers/base_trainer.py
+++ ocpmodels/trainers/base_trainer.py
@@ -388,7 +388,7 @@ class BaseTrainer(ABC):
         
         #load checkpoint -> resume training
         # try:
-        _checkpoint = torch.load('/content/drive/MyDrive/RESEARCH/CuO/pretrained_models/cgcnn_all.pt', map_location=self.device)
+        _checkpoint = torch.load('/content/drive/MyDrive/RESEARCH/CuO/pretrained_models/dimenetpp_all.pt', map_location=self.device)
         new_state_dict = OrderedDict()
         for k, v in _checkpoint['state_dict'].items():
             name = k[14:] # remove module.module.
